d9fa5b1b5f7da90676112aa7789e036eac4d3db1
tjsdud2671@naver.com <huy5infor@gmail.com>
2024-07-06 19:41:18 +0900

    20240706_0741

--- js/modal.js
+++ js/modal.js
@@ -1,6 +1,11 @@
 const modal = document.querySelector('.modal');
 const btnOpenModal = document.querySelector('.btn-open-modal');
+const btnDeskModal = document.querySelector('.btn-desk-modal');
 
 btnOpenModal.addEventListener('click', () => {
+  modal.style.display = 'flex';
+});
+
+btnDeskModal.addEventListener('click', () => {
   modal.style.display = 'flex';
 });
